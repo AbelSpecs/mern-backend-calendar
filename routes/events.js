45a6e846102645543c083eab79eebc080946b44e
AbelSpecs <abdv.vargas@gmail.com>
2021-04-02 17:11:32 -0400

    agregado helper isdate

--- routes/events.js
+++ routes/events.js
@@ -3,6 +3,7 @@ const { Router } = require('express');
 const router = Router();
 const {check} = require('express-validator');
 const {getEventos, crearEvento, actualizarEvento, borrarEvento} = require('../controllers/events');
+const { isDate } = require('../helpers/isDate');
 const { validarCampos } = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
 
@@ -17,8 +18,8 @@ router.get('/', getEventos);
 router.post('/', 
             [
                 check('title', 'el titulo es obligatorio').not().isEmpty(),
-                check('start', 'la fecha inicial es obligatoria o debe ser una fecha').isDate(),
-                check('end', 'la fecha final es obligatoria o debe ser una fecha').isDate(),  
+                check('start', 'la fecha inicial es obligatoria o debe ser una fecha').custom(isDate),
+                check('end', 'la fecha final es obligatoria o debe ser una fecha').custom(isDate),  
                 validarCampos
             ],
             crearEvento);
@@ -26,7 +27,7 @@ router.post('/',
 // actualizar evento
 
 router.put('/:id', 
-            [
+            [ 
                 check('title', 'el titulo es obligatorio').not().isEmpty(),
                 check('start', 'la fecha inicial es obligatoria o debe ser una fecha').isDate(),
                 check('end', 'la fecha final es obligatoria o debe ser una fecha').isDate(),  
